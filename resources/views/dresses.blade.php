771abd8cbde0aec3d7705d64ec4c2d2f36f33c88
Matteo <matteomigliori280489@gmail.com>
2021-05-14 15:46:10 +0200

    push

---
+++ resources/views/dresses.blade.php
@@ -0,0 +1,9 @@
+@extends ('layouts.app')
+
+@section('title','vestiti')
+
+@section ('content')
+
+    <h1>Lista Nostri vestiti</h1>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends ('layouts.app')
+
+@section('title','home-page')
+
+@section ('content')
+
+    <h1>Homepage</h1>
+
+@endsection
